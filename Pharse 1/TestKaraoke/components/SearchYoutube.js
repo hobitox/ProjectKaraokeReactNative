e89d8504a0b9dce130b14cea04230efe8eb544b0
nndb05123 <duybao05123@gmail.com>
2018-12-11 23:13:32 +0700

    Pharse 1 fix

---
+++ Pharse 1/TestKaraoke/components/SearchYoutube.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { View, TextInput,Button } from 'react-native';
+
+class SearchYoutube extends Component {
+  state = { term: '' };
+
+  render() {
+    const {
+      containerStyle,
+      searchTextStyle,
+      buttonStyle
+    } = styles;
+
+    return (
+      <View style={containerStyle}>
+        <TextInput
+          style={searchTextStyle}
+          onChangeText={term => this.setState({ term })}
+          value={this.state.term}
+        />
+        <Button
+          buttonStyle={buttonStyle}
+          title={this.props.loading ? 'Loading...' : 'Search'}
+          onPress={() => this.props.onPressSearch(this.state.term)}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = {
+  containerStyle: {
+    marginTop: 75,
+    marginLeft: 10,
+    marginRight: 10,
+    flexDirection: 'row'
+  },
+  searchTextStyle: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  buttonStyle: {
+    height: 30,
+    marginBottom: 8
+  }
+};
+
+
+export default SearchYoutube;
